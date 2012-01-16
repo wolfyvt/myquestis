14d269244717ff43e406177a3beff9b27431b53f
Shane Wolf <shane@grockit.com>
2012-01-16 12:53:02 -0500

    codes

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,9 @@
+class SiteController < ApplicationController
+  def index
+    
+  end
+  
+  def get_started
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myquestis::Application.config.secret_token = '2e0c216378e0e58af0bb3eb5d61eace87475423ee07d75868e1095d5e53a027592f3c26d6f727215fe67b1b23ead10f823a76f661a560b0183073c09b7d1de80'
